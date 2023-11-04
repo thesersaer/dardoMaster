a903960b3ca384ee6e2a375cab27bdaac93406d1
thesersaer <sersaer@gmail.com>
2023-11-04 17:01:52 +0100

    Moving

--- model/definitions.py
+++ model/definitions.py
@@ -1,3 +1,7 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).strip('\\model')
+
 grid_cols = [15, 16, 17, 18, 19, 20, 25]
 display_grid_cols = ['J', 'PTS', '15', '16', '17', '18', '19', '20', '🎯']
 grid_keys = ['J', 'PTS'] + grid_cols
--- model/gameModel.py
+++ model/gameModel.py
@@ -25,8 +25,7 @@ class Throw:
 
 
 class Player:
-    def __init__(self, rules: Rules, name: str = 'Player'):
-        self.rules = rules
+    def __init__(self, name: str = 'Player'):
         self.name = name
         self.penalty_rounds = 0
         self.throws = 3
@@ -86,8 +85,8 @@ class CricketPlayer(Player):
         key: True for key in definitions.cricket_scoring_numbers
     }
 
-    def __init__(self, rules: CricketRules, name: str = 'Player'):
-        super(CricketPlayer, self).__init__(rules, name)
+    def __init__(self, name: str = 'Player'):
+        super(CricketPlayer, self).__init__(name)
 
         self.numbers = {
             key: -3 for key in definitions.cricket_scoring_numbers
@@ -98,54 +97,14 @@ class CricketPlayer(Player):
         for number in CricketPlayer.available_numbers:
             CricketPlayer.available_numbers[number] = True
 
-
-class Rules:
-    @staticmethod
-    def get_player_score(player: Player):
-        pass
-
-
-class CricketRules(Rules):
-
-    @staticmethod
-    # TODO: Block scores once number is not available
-    def get_player_score(player: CricketPlayer):
-        numbers_buffer = player.numbers.copy()
-        numbers_buffer = dict.fromkeys(numbers_buffer, -3)
-        score_buffer = 0
-
-        for throw in player.throw_log:
-            if throw.score in definitions.cricket_scoring_numbers:
-                numbers_buffer[throw.score] += throw.modifier
-
-        for key, value in numbers_buffer.items():
-            if value > 0:
-                score_buffer += key * value
-
-        player.numbers = numbers_buffer
-        player.score = score_buffer
-
-    @staticmethod
-    def check_winner(player_list: typing.List[CricketPlayer]):
-        winners = []
-        scores_maximum = max([player.score for player in player_list])
-        for index, player in enumerate(player_list):
-            score_condition = player.score == scores_maximum
-            numbers_condition = all([value >= 0 for value in player.numbers])
-            if score_condition and numbers_condition:
-                winners.append(index)
-        return winners
-
-    @staticmethod
-    def check_available_numbers(player_list: typing.List[CricketPlayer]):
-        for number in definitions.cricket_scoring_numbers:
-            closed_number = [player.numbers[number] >= 0 for player in player_list]
-            if all(closed_number):
-                CricketPlayer.available_numbers[number] = False
+    def reset_player_numbers(self):
+        for key in self.numbers:
+            self.numbers[key] = -3
 
 
 class Score:
-    pass
+    def __init__(self, player_list: typing.List[Player]):
+        self.player_list = player_list
 
     def check_winner(self):
         pass
@@ -153,10 +112,14 @@ class Score:
     def update_player_score(self, player_index: int):
         pass
 
+    def player_update_sequence(self, player_index: int):
+        pass
+
 
 class CricketScore(Score):
     def __init__(self, player_list: typing.List[CricketPlayer]):
-        self.player_list = player_list
+        super().__init__(player_list)
+        self.player_list: typing.List[CricketPlayer] = player_list
 
         self.top_score = set()
         self.all_numbers_closed = set()
@@ -186,7 +149,7 @@ class CricketScore(Score):
         if winners:
             return True, winners
         else:
-            return False
+            return False, []
 
     def _check_closed_numbers(self):
         for number in definitions.cricket_scoring_numbers:
@@ -199,13 +162,23 @@ class CricketScore(Score):
 
     def update_player_numbers_from_last_throw(self, player_index: int):
         player = self.player_list[player_index]
-        throw = player.throw_log[-1]
-        if throw.score in definitions.cricket_scoring_numbers:
-            if player.numbers[throw.score] < 0:
-                player.numbers[throw.score] += 1
-            else:
-                if throw.score not in self.closed_numbers:
-                    player.numbers[throw.score] += 1
+        try:
+            throw = player.throw_log[-1]
+            if throw.score in definitions.cricket_scoring_numbers:
+                player_number = player.numbers[throw.score]
+
+                if throw.score in self.closed_numbers and player_number < 0:
+                    player_number_final = player_number + throw.modifier
+                    if player_number_final > 0:
+                        player.numbers[throw.score] = 0
+                    else:
+                        player.numbers[throw.score] = player_number_final
+
+                elif throw.score not in self.closed_numbers:
+                    player.numbers[throw.score] += throw.modifier
+
+        except IndexError:
+            player.reset_player_numbers()
 
     def update_player_score(self, player_index: int):
         player = self.player_list[player_index]
@@ -215,49 +188,70 @@ class CricketScore(Score):
                 score_buffer += key * value
         player.score = score_buffer
 
+    def player_update_sequence(self, player_index: int):
+        self.update_player_numbers_from_last_throw(player_index)
+        self.update_player_score(player_index)
+        self._check_closed_numbers()
+        self._check_all_closed()
+        self._check_top_score()
+        return self.check_winner()
+
 
 class Game:
-    def __init__(self, players: typing.List[Player]):
+    def __init__(self, player_list: typing.List[Player], score: typing.Type[Score]):
+        self.score_type = score
+        self.score = score(player_list)
         self.turn = 0
         self.round = 0
-        self.players: typing.List[Player] = players
+        self.player_list: typing.List[Player] = player_list
         self.turns_skipped = 0
+        self.winners = (False,[])
 
-    def add_throw(self, throw: Throw, player_position: int):
-        player = self.players[player_position]
+    def add_throw(self, throw: Throw, player_index: int = None):
+        if player_index is None:
+            player_index = self.turn
+        player = self.player_list[player_index]
         if player.add_throw(throw):
             if not throw.out and not throw.foul:
-                player.rules.get_player_score(player)
+                self.winners = self.score.player_update_sequence(player_index)
             return True
         return False
 
-    def remove_throw(self, throw: Throw, player_position: int):
-        player = self.players[player_position]
+    def remove_throw(self, throw: Throw, player_index: int):
+        player = self.player_list[player_index]
         if player.remove_throw(throw):
             if not throw.out and not throw.foul:
-                player.rules.get_player_score(player)
+                self.winners = self.score.player_update_sequence(player_index)
             return True
         return False
 
     def undo_throw(self):
-        player = self.players[self.turn]
-        throw = player.throw_log[-1]
-        if player.undo_throw():
-            if not throw.out and not throw.foul:
-                player.rules.get_player_score(player)
-            return True
-        return False
+        player = self.player_list[self.turn]
+        try:
+            throw = player.throw_log[-1]
+            if player.undo_throw():
+                if not throw.out and not throw.foul:
+                    self.winners = self.score.player_update_sequence(self.turn)
+                return True
+            return False
+        except IndexError:
+            return False
+
+    def has_throws_left(self, player_index: int = None):
+        if player_index is None:
+            player_index = self.turn
+        return self.player_list[player_index].throws > 0
 
     def reset_throw_count(self, player=None):
         if player is None:
-            for player in self.players:
+            for player in self.player_list:
                 player.throws = 3
         else:
             if isinstance(player, int):
-                self.players[player].throws = 3
+                self.player_list[player].throws = 3
 
     def next_turn(self):
-        if self.turn < len(self.players) - 1:
+        if self.turn < len(self.player_list) - 1:
             self.turn += 1
             return True
         return False
@@ -269,7 +263,7 @@ class Game:
         return False
 
     def undo_turn(self):
-        player = self.players[self.turn]
+        player = self.player_list[self.turn]
         if player.throws < 3:
             for _ in range(3 - player.throws):
                 self.undo_throw()
@@ -277,7 +271,7 @@ class Game:
         return False
 
     def next_round(self):
-        if self.turn == len(self.players) - 1:
+        if self.turn == len(self.player_list) - 1:
             self.turn = 0
             self.round += 1
             return True
@@ -298,17 +292,54 @@ class GameManager:
         self.game = game
 
         self.started = False
+        self.winners = None
 
     def start_game(self):
-        pass
+        if not self.started:
+            self.game.score = self.game.score_type(self.game.player_list)
+            self.game.turn = 0
+            self.game.round = 0
+            self.game.turns_skipped = 0
+            self.started = True
+
+    def add_throw(self, throw: Throw):
+        if self.started:
+            self.game.add_throw(throw)
+            if self.game.winners[0]:
+                self.winners = self.game.winners[1]
+                self.end_game()
+
+    def undo_throw(self):
+        if self.started:
+            return self.game.undo_throw()
+
+    def end_game(self):
+        if self.started:
+            self.started = False
+
+    def next_turn(self):
+        if self.started:
+            if not self.game.has_throws_left():
+                if not self.game.next_turn():
+                    if not self.game.next_round():
+                        return False
+                    else:
+                        self.game.reset_throw_count()
+
+    def undo_turn(self):
+        return self.game.undo_turn()
+
+    def undo_round(self):
+        return self.game.undo_round()
 
 
 def main():
-    rules = CricketRules()
-    p1 = CricketPlayer(rules, 'P1')
-    p2 = CricketPlayer(rules, 'P2')
+    p1 = CricketPlayer('P1')
+    p2 = CricketPlayer('P2')
 
-    game = Game([p1, p2])
+    game = Game([p1, p2], CricketScore)
+    game_manager = GameManager(game)
+
     t1 = Throw(15, 1)
     t2 = Throw('f')
     t3 = Throw(15, 3)
@@ -325,28 +356,30 @@ def main():
 
     tl = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10]
 
-    game.add_throw(t1)
-    game.add_throw(t2)
-    game.add_throw(t3)
+    game_manager.start_game()
 
-    game.advance_play()
-    game.add_throw(t2)
-    game.add_throw(t4)
-    game.add_throw(t5)
+    game_manager.add_throw(t1)
+    game_manager.add_throw(t2)
+    game_manager.add_throw(t3)
 
-    game.advance_play()
-    game.add_throw(t6)
-    game.add_throw(t7)
-    game.add_throw(t9)
+    game_manager.next_turn()
+    game_manager.add_throw(t2)
+    game_manager.add_throw(t4)
+    game_manager.add_throw(t5)
 
-    game.advance_play()
-    game.undo_turn()
+    game_manager.next_turn()
+    game_manager.add_throw(t6)
+    game_manager.add_throw(t7)
+    game_manager.add_throw(t9)
+
+    game_manager.next_turn()
+    game_manager.undo_turn()
 
     input('END')
 
 
 def main2():
-    p1 = Player(CricketRules(), 'P1')
+    p1 = Player('P1')
     t1 = Throw(15, 2)
     t2 = Throw(15, 2)
     t3 = Throw('f')
@@ -359,5 +392,14 @@ def main2():
     input('END')
 
 
+def main3():
+    import os
+    import time
+    for ii in range(4):
+        print(f'TEST{ii}')
+        time.sleep(1)
+        os.system('cls')
+
+
 if __name__ == '__main__':
-    main2()
+    main3()
