7daf3973b055573ba483600f17f9bf14485e57e4
thesersaer <sersaer@gmail.com>
2023-11-05 23:56:03 +0100

    Edit definitions.py

--- model/definitions.py
+++ model/definitions.py
@@ -2,12 +2,23 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).strip('\\model')
 
-grid_cols = [15, 16, 17, 18, 19, 20, 25]
-display_grid_cols = ['J', 'PTS', '15', '16', '17', '18', '19', '20', '🎯']
-grid_keys = ['J', 'PTS'] + [str(number) for number in grid_cols]
+# GENERAL
 
 dartboard_numbers = [number for number in range(1, 21, 1)]
 
+foul_penalty_rounds = 2
+
+min_players = 2
+
+max_players = 6
+
+# CRICKET
+
 cricket_scoring_numbers = [15, 16, 17, 18, 19, 20, 25]
 
-foul_penalty_rounds = 2
+cricket_row_header = ['J', 'PTS'] + [str(number) for number in cricket_scoring_numbers]
+
+cricket_number_0_str = '○○○'
+cricket_number_1_str = '●○○'
+cricket_number_2_str = '●●○'
+cricket_number_3_str = '●●●'
